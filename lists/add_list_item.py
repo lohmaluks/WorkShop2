fef8c513c4c5be2e076497e9a69ecc610b85a033
lohmaluks <ymineaowaluk@gmail.com>
2021-01-17 10:03:22 +0700

    add new folder and new file ,ex_1_list buddy aus

---
+++ lists/add_list_item.py
@@ -0,0 +1,10 @@
+# ex.1
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+
+print(thislist)
+
+# ex.2
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
---
+++ lists/loop_list.py
@@ -0,0 +1,4 @@
+fruit_list = ["apple", "banana", "cherry"]
+for fruit in fruit_list:
+    print(fruit)
+    
